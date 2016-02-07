0893c98935f3bde2819c57d676ec4cd779be040a
Lyons Software <admin@lyonssoftware.com>
2016-02-07 15:29:08 -0600

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,6 @@
+<?php
+
+  // create an array called vehicles
+  $vehicles = array("BMW", "Honda", "Bentley");
+  echo "I own a " . $vehicles[0] . ", " . $vehicles[1] . " and " . $vehicles[2] . ".";
+?>
