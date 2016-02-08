fe20c7cc19a241c5ff02a092532b34c1a7a9e482
Lyons Software <admin@lyonssoftware.com>
2016-02-08 16:15:22 -0600

    Create dataTypes.php

---
+++ dataTypes.php
@@ -0,0 +1,18 @@
+<?php
+  // php supports several datatypes
+  // String
+  $string = "This is a string";
+  
+  // Integer
+  $int = 35; 
+  
+  // Float (floating point numbers - also called double)
+  $float = 3.25;
+  
+  // Boolean
+  $bool = True;
+  $bool2 = False;
+  
+  // Array
+  $months = array(1 => 'January', 'February', 'March');
+?>
