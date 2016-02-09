37a2f89960d9c714445bf7b0a635dc2a5c54e0a5
Lyons Software <admin@lyonssoftware.com>
2016-02-09 12:49:32 -0600

    Create swap.php

---
+++ swap.php
@@ -0,0 +1,18 @@
+<?php
+  // swap variable values in php
+  $one = 1;
+  $two = 2;
+  
+  // print variables 
+  echo $one;
+  echo $two;
+  
+  // swap
+  $temp = $one;
+  $one = $two;
+  $two = $temp;
+  
+   // print new variables 
+  echo $one;
+  echo $two;
+?>
