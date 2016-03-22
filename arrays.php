ef9112edf08b1300e35a5bfde93a7bcbe279c691
Lyons Software <admin@lyonssoftware.com>
2016-03-21 20:27:20 -0500

    Update arrays.php

--- arrays.php
+++ arrays.php
@@ -1,6 +1,6 @@
 <?php
   // arrays are data strutucres to hold sequential data
   // create an array called vehicles
-  $vehicles = array("BMW", "Honda", "Bentley", "Mercedes");
+  $vehicles = array("BMW", "Honda", "Bentley", "Mercedes", "Audi");
   echo "I own a " . $vehicles[0] . ", " . $vehicles[1] . " and " . $vehicles[2] . ".";
 ?>
