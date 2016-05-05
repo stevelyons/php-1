3e3cda64f82244e865e0bae229e5e8401513003e
Lyons Software <admin@lyonssoftware.com>
2016-05-04 21:11:13 -0500

    Update sortNumbers.php

--- sortNumbers.php
+++ sortNumbers.php
@@ -1,5 +1,5 @@
 <?php
-  // sort in array in ascending order from lowest to highest
+  // sort in array in ascending order from low to high 
   $numbers = array(4, 6, 2, 22, 11);
   sort($numbers);
 ?> 
