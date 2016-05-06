f902f2fc824975bde0bef2c8a25c1fd3f5766013
Lyons Software <admin@lyonssoftware.com>
2016-05-05 22:52:16 -0500

    Update sortByKey.php

--- sortByKey.php
+++ sortByKey.php
@@ -1,5 +1,5 @@
 <?php
-  // assign key to each array element in order to set to sort array
+  // assign key to each element in order to set to sort array
   $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
   ksort($age);
 ?> 
