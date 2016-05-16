46a7d702999356697f98f08c8b0cf78c6add13ef
Lyons Software <admin@lyonssoftware.com>
2016-05-15 21:38:35 -0500

    Update if.php

--- if.php
+++ if.php
@@ -1,7 +1,7 @@
 <?php
 $t = date("H");
 // conditional if statement
-// if the conditions are met, the statement will proceed
+// if the conditions are met, the statement will proceed, if not then the statement will not 
 if ($t < "20") {
     echo "Have a good day!";
 }
