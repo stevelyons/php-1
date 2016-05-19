659b418344b67385b04984d5339da833946f0687
Lyons Software <admin@lyonssoftware.com>
2016-05-18 22:16:14 -0500

    Update global.php

--- global.php
+++ global.php
@@ -2,7 +2,7 @@
 $x = 75;
 $y = 25;
  
- // global variables are available in multiple scopes through out php
+ // global variables are available in multiple scopes through out the php program 
 function addition() {
     $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
 }
