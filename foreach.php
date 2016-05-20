dfe4ccd06d30ec93fcb84845b6a8e0a9c6b5872e
Lyons Software <admin@lyonssoftware.com>
2016-05-19 22:23:34 -0500

    Update foreach.php

--- foreach.php
+++ foreach.php
@@ -1,6 +1,6 @@
  <?php
  // array of colors 
-  $colors = array("red", "green", "blue", "yellow");
+  $colors = array("red", "green", "blue", "yellow", "purple", "orange");
   
   // a modified for loop, foreach loops are used  to iterate through all colors in array 
   foreach ($colors as $value) 
